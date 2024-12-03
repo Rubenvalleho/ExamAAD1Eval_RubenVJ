5371ef2df83840910d9cd4ca64faafdecd93e7ab
Rubén Vallejo <rubenvalleho@gmail.com>
2024-12-03 20:30:36 +0100

    Ejercicio 1 completo

--- app/src/main/java/edu/iesam/examaad1eval/MainActivity.kt
+++ app/src/main/java/edu/iesam/examaad1eval/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.iesam.examaad1eval
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import edu.iesam.examaad1eval.features.ex1.data.Ex1DataRepository
+import edu.iesam.examaad1eval.features.ex1.data.local.Ex1XmlLocalDataSource
+import edu.iesam.examaad1eval.features.ex1.data.remote.MockEx1RemoteDataSource
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -11,15 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         executeExercise1()
-        executeExercise2()
+        //executeExercise2()
     }
 
-    private fun executeExercise1(){
-        //Ejecutar el ejercicio 1 desde aquí llamando al Ex1DataRepository directamente
+    private fun executeExercise1() {
+        val ex1DataRepository = Ex1DataRepository(
+            Ex1XmlLocalDataSource(this),
+            MockEx1RemoteDataSource()
+        )
+
+        ex1DataRepository.getUsers()
+        ex1DataRepository.getItems()
+        ex1DataRepository.getServices()
     }
 
     @OptIn(DelicateCoroutinesApi::class)
-    private fun executeExercise2(){
+    private fun executeExercise2() {
         //Ejecutar el ejercicio 2 desde aquí llamando al Ex2DataRepository directamente
         GlobalScope.launch {
             //llamar a Room
