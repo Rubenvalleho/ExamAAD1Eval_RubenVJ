21b266ec32a216382f28647edbef5691fb6a24a5
Rubén Vallejo <rubenvalleho@gmail.com>
2024-12-03 20:34:17 +0100

    Añadido visualizar por pantalla

--- app/src/main/java/edu/iesam/examaad1eval/MainActivity.kt
+++ app/src/main/java/edu/iesam/examaad1eval/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.iesam.examaad1eval
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import edu.iesam.examaad1eval.features.ex1.data.Ex1DataRepository
 import edu.iesam.examaad1eval.features.ex1.data.local.Ex1XmlLocalDataSource
@@ -23,9 +24,9 @@ class MainActivity : AppCompatActivity() {
             MockEx1RemoteDataSource()
         )
 
-        ex1DataRepository.getUsers()
-        ex1DataRepository.getItems()
-        ex1DataRepository.getServices()
+        Log.d("dev", ex1DataRepository.getUsers().toString())
+        Log.d("dev", ex1DataRepository.getItems().toString())
+        Log.d("dev", ex1DataRepository.getServices().toString())
     }
 
     @OptIn(DelicateCoroutinesApi::class)
